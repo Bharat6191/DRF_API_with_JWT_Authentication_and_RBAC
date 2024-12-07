773317e37744bd89f74a8a0897a7db95a58d491d
Bharat6191 <bharatsolanke111@gmail.com>
2024-12-07 14:52:46 +0530

    initial commit10

--- Authentication/account/urls.py
+++ Authentication/account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegisterView, LoginView, ProjectView, AdminProjectView,LogoutView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    path('', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('projects/', ProjectView.as_view(), name='get_projects'),
     path('projects/create/', AdminProjectView.as_view(), name='create_project'),
